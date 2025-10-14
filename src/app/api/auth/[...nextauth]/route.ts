17c23334dafe4d00f488b3d57772ff7948c3fa64
Visshva Ramasubramanian <186075605+visshva-r@users.noreply.github.com>
2025-10-14 23:36:46 +0530

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,7 @@
 // src/app/api/auth/[...nextauth]/route.ts
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
