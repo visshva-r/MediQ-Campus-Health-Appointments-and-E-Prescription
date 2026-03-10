dfd7d47c2989f5da03e5a7807df0e802e76de88c
Visshva Ramasubramanian <186075605+visshva-r@users.noreply.github.com>
2026-03-11 00:30:17 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Don’t block production build on ESLint warnings/errors
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // Don’t block production build on TS errors
-    ignoreBuildErrors: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
